25de1b1a25d8d0faada2caec78338988d42e8d28
goldentechie <bendam1207@gmail.com>
2017-01-05 11:31:35 +0530

    policy document check on login

--- src/containers/user/monthlyAttendance.jsx
+++ src/containers/user/monthlyAttendance.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import {Router, browserHistory, Link, withRouter} from 'react-router'
-import * as actions_monthlyAttendance from '../../actions/user/monthlyAttendance'
+
 import * as _ from 'lodash'
 import {notify} from '../../services/index'
-
 import Menu from '../../components/generic/Menu'
 import LoadingIcon from '../../components/generic/LoadingIcon'
 
 import * as actions_login from '../../actions/login/index'
 import * as actions_userDaySummary from '../../actions/user/userDaySummary'
+import * as actions_monthlyAttendance from '../../actions/user/monthlyAttendance'
+import * as actions_policy from '../../actions/policyDocuments/index'
+import * as actions_myProfile from '../../actions/user/myProfile'
+
 import {CONFIG} from '../../config/index'
 
 import UserMonthlyAttendance from '../../components/attendance/UserMonthlyAttendance'
@@ -24,7 +27,8 @@ class MonthlyAttendance extends React.Component {
       "daysummary_date": "",
       year: "",
       month: '',
-      test: "show"
+      test: "show",
+      userDoc:['bvnvbn','efce','vbnvb','vbnvb']
     }
 
     this.onShowDaySummary = this.onShowDaySummary.bind(this)
@@ -33,7 +37,9 @@ class MonthlyAttendance extends React.Component {
   }
   componentWillMount() {
     this.props.onIsAlreadyLogin()
-    console.log(localStorage.getItem("userid"));
+    this.props.onFetchPolicyDocument();
+    this.props.onMyProfileDetails();
+
     let user_id = this.props.logged_user.userid;
     this.setState({"defaultUserDisplay": user_id})
     let d = new Date();
@@ -49,6 +55,14 @@ class MonthlyAttendance extends React.Component {
     } else {
       if (props.logged_user.role == CONFIG.ADMIN || props.logged_user.role == CONFIG.GUEST) {
         this.props.router.push('/home');
+      }else{
+        _.map(props.policy_documents.policyDocuments,(doc)=>{
+          console.log('MonthlyAttendance called',_.includes(this.state.userDoc,doc.name),doc.name);
+          if(!_.includes(this.state.userDoc,doc.name)){
+            console.log('redirected to policy_documents');
+            this.props.router.push('/policy_documents');
+          }
+        });
       }
     }
   }
@@ -62,12 +76,12 @@ class MonthlyAttendance extends React.Component {
   }
   render() {
     let mainDivs = <div className="row">
+                      <div className="col-md-1"></div>
+                      <div className="col-md-10">
+                        <UserMonthlyAttendance {...this.props} monthToggle={this.monthToggle} onShowDaySummary={this.onShowDaySummary}/>
+                      </div>
+                   </div>
 
-      <div className="col-md-1"></div>
-      <div className="col-md-10">
-        <UserMonthlyAttendance {...this.props} monthToggle={this.monthToggle} onShowDaySummary={this.onShowDaySummary}/>
-      </div>
-    </div>
     return (
       <div >
         <Menu {...this.props}/>
@@ -111,7 +125,14 @@ MonthlyAttendance.styles = {
 };
 
 function mapStateToProps(state) {
-  return {frontend: state.frontend.toJS(), userDaySummary: state.userDaySummary.toJS(), logged_user: state.logged_user.toJS(), monthlyAttendance: state.monthlyAttendance.toJS()}
+  return {
+    frontend: state.frontend.toJS(),
+    userDaySummary: state.userDaySummary.toJS(),
+    logged_user: state.logged_user.toJS(),
+    monthlyAttendance: state.monthlyAttendance.toJS(),
+    myProfile: state.myProfile.toJS(),
+    policy_documents: state.policyDocuments.toJS(),
+  }
 }
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -127,7 +148,13 @@ const mapDispatchToProps = (dispatch) => {
     },
     onUserUpdateDaySummary: (userid, date, entry_time, exit_time, reason, year, month) => {
       return dispatch(actions_userDaySummary.userUpdateUserDaySummary(userid, date, entry_time, exit_time, reason, year, month))
-    }
+    },
+    onFetchPolicyDocument: ()=>{
+      return dispatch(actions_policy.fetchPolicyDocument());
+    },
+    onMyProfileDetails: () => {
+      return dispatch(actions_myProfile.getMyProfileDetails())
+    },
   }
 }
 
