f06e5c37ec9d1a6dcf6a1e534131994304b3daa9
goldentechie <bendam1207@gmail.com>
2017-06-07 16:43:38 +0530

    manage user module completed

--- src/components/inventory/ViewUser.jsx
+++ src/components/inventory/ViewUser.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import * as _ from 'lodash'
-import LoadingIcon from '../../components/generic/LoadingIcon'
-import Paper from 'material-ui/Paper'
-var moment = require('moment')
-import { CONFIG } from '../../config/index'
-import UsersList from '../../components/generic/UsersList'
+import React from 'react';
+import { connect } from 'react-redux';
+import * as _ from 'lodash';
+import LoadingIcon from '../../components/generic/LoadingIcon';
+import Paper from 'material-ui/Paper';
+var moment = require('moment');
+import { CONFIG } from '../../config/index';
 
 export default class ViewUserDevice extends React.Component {
   constructor (props) {
-    super(props)
+    super(props);
   }
 
   render () {
-    let rows = []
+    let rows = [];
     _.map(this.props.manageUsers.user_assign_machine, (device, i) => {
       rows.push(<tr key={i}>
             <td style={{marginRight: '0%'}}>{i + 1}</td>
@@ -22,12 +21,13 @@ export default class ViewUserDevice extends React.Component {
             <td>{device.mac_address}</td>
             <td>{device.assign_date}</td>
           </tr>
-        )
-    })
+        );
+    });
     return (
       <div>
         <div className="col-md-9">
           <h6 className="text-center">Asssigned Device Details</h6>
+          <hr />
           <br />
 
           <div className='row'>
@@ -53,9 +53,13 @@ export default class ViewUserDevice extends React.Component {
                          </tbody>
                        </table>
                     </Paper>
-                    : <div className="well">
-                    <h5 style={{marginLeft: '42%'}}>{'Device not Asssigned'}</h5>
-                  </div>
+                    : <div className="col-xs-6 col-xs-offset-3 well">
+                    <i className="fa fa-exclamation-triangle fa-2x"
+                      style={{marginLeft: '45%', opacity: '0.56'}} aria-hidden="true"></i>
+                    <h5 style={{marginLeft: '33%', opacity: '0.56'}}>
+                    {'Device not Asssigned !'}
+                  </h5>
+                </div>
                   }
                   </div>
                 </div>
@@ -63,6 +67,6 @@ export default class ViewUserDevice extends React.Component {
 
     </div>
       </div>
-    )
+    );
   }
 }
