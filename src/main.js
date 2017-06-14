21829dc0eb3b98a37951a65110086accf6b8949e
goldentechie <bendam1207@gmail.com>
2017-06-14 15:01:36 +0530

    dist folder of static files removed from gitignore & console removed from webpack

--- src/main.js
+++ src/main.js
@@ -45,8 +45,6 @@ import Page_MyDocuments from './modules/myDocuments/containers/myDocuments';
 import PageManageUsers from './modules/manageUsers/containers/manageUsers';
 import Page_ManageWorkingHours from './modules/workingHours/containers/manageWorkingHours';
 import Page_ManageUserWorkingHours from './modules/workingHours/containers/manageUserWorkingHours';
-import Page_ManageUserPendingHours from './modules/workingHours/containers/manageUserPendingHours';
-
 // -----------------------------
 
 // -admin
@@ -141,7 +139,6 @@ applyMiddleware(thunk),
 
 // --start----added by arun for HR app
 let render = (routerKey = null) => {
-  // console.log('aaaaa');
   ReactDOM.render((
     <MuiThemeProvider>
       <Provider store={store}>
@@ -160,7 +157,6 @@ let render = (routerKey = null) => {
             <Route path="my_leaves" component={Page_MyLeaves} />
             <Route path="disabled_employes" component={PageDisabledEmployes} />
             <Route path="manage_user_working_hours" component={Page_ManageUserWorkingHours} />
-            <Route path="manage_user_pending_hours" component={Page_ManageUserPendingHours} />
             <Route path="leaves_summary" component={Page_LeavesSummary} />
             <Route path="salary" component={Page_Salary} />
             <Route path="manage_salary" component={Page_ManageSalary} />
