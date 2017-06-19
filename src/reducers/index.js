e4bd314e0ba803b7c277a7e44443a0c40ddeed36
goldentechie <bendam1207@gmail.com>
2017-06-19 12:17:49 +0530

    modifications with webpack2

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -18,7 +18,7 @@ import { salary } from 'appRedux/salary/reducers/salary';
 import { manageSalary } from 'appRedux/salary/reducers/manageSalary';
 import { myProfile } from './user/myProfile';
 import { myDocument } from './user/myDocument';
-import { manageUsers } from 'appRedux/manageUsers/reducers/manageUsers';
+import { manageUsers } from './admin/manageUsers';
 import { clientsList } from './admin/clientsList';
 import { manageClients } from './admin/manageClients';
 import { managePayslips } from 'appRedux/salary/reducers/managePayslips';
@@ -26,7 +26,7 @@ import { empSalaryList } from 'appRedux/salary/reducers/empSalaryDetails';
 import {policyDocuments} from 'appRedux/policyDocuments/reducers/policyDocuments';
 import {template} from './admin/template';
 import {teamList} from './admin/teamList';
-import {manageDevice} from 'appRedux/inventory/reducers/inventory';
+import {manageDevice} from './admin/inventory';
 
 export default combineReducers({
   frontend,
