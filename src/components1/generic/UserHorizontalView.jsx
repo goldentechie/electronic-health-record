8c6033bb8b936e2b9c447f4294a272bb09e8e1ae
goldentechie <bendam1207@gmail.com>
2017-06-27 19:20:38 +0530

    inventory module completed

--- src/components1/generic/UserHorizontalView.jsx
+++ src/components1/generic/UserHorizontalView.jsx
@@ -1,69 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserHorizontalView extends React.Component {
-  constructor (props) {
-    super(props);
+const UserHorizontalView = ({profileImage, name, jobtitle, dateofjoining, gender, dob, work_email, inventory}) => {
+  let joining, details;
+  if (!inventory) {
+    joining = <p className="text-muted">
+      <span className="m-r">Joining Date : {this.props.dateofjoining}</span>
+    </p>;
+    details = <div className="col-sm-5">
+      <p className="text-muted">
+        <span className="m-r">Gender :<b>{this.props.gender}</b></span>
+      </p>
+      <p className="text-muted">
+        <span className="m-r">Date Of Birth :<b>{this.props.dob}</b></span>
+      </p>
+      <p className="text-muted">
+        <span className="m-r">Work Email :<b>{this.props.work_email}</b></span>
+      </p>
+    </div>;
   }
-  componentWillReceiveProps (props) {}
-
-  render () {
-    return (
-      <div className="item">
-        <div className="item-bg">
-          <img src={this.props.profileImage} className="blur opacity-3" />
-        </div>
-        <div className="p-a-md">
-          <div className="row m-t">
-            <div className="col-sm-7">
-              <a href="" className="pull-left m-r-md">
-                <span className="avatar w-96">
-                  <img src={this.props.profileImage} />
-                  <i className="on b-white"></i>
-                </span>
-              </a>
-              <div className="clear m-b">
-                <h3 className="m-a-0 m-b-xs">{this.props.name}</h3>
-                <p className="text-muted">
-                  <span className="m-r">{this.props.jobtitle}</span>
-                </p>
-                {this.props.inventory ? null
-                  : <p className="text-muted">
-                    <span className="m-r">Joining Date : {this.props.dateofjoining}</span>
-                  </p>
-                }
-              </div>
+  return (
+    <div className="item">
+      <div className="item-bg">
+        <img src={profileImage} className="blur opacity-3" />
+      </div>
+      <div className="p-a-md">
+        <div className="row m-t">
+          <div className="col-sm-7">
+            <a href="" className="pull-left m-r-md">
+              <span className="avatar w-96">
+                <img src={profileImage} />
+                <i className="on b-white"></i>
+              </span>
+            </a>
+            <div className="clear m-b">
+              <h3 className="m-a-0 m-b-xs">{name}</h3>
+              <p className="text-muted">
+                <span className="m-r">{jobtitle}</span>
+              </p>
+              {joining}
             </div>
-            {this.props.inventory ? null
-              : <div className="col-sm-5">
-                <p className="text-muted">
-                  <span className="m-r">Gender :
-                    <b>{this.props.gender}</b>
-                  </span>
-                </p>
-                <p className="text-muted">
-                  <span className="m-r">Date Of Birth :
-                    <b>{this.props.dob}</b>
-                  </span>
-                </p>
-                <p className="text-muted">
-                  <span className="m-r">Work Email :
-                    <b>{this.props.work_email}</b>
-                  </span>
-                </p>
-              </div>
-            }
           </div>
+          {details}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 UserHorizontalView.propTypes = {
-  name: React.PropTypes.string,
-  jobtitle: React.PropTypes.string,
-  dateofjoining: React.PropTypes.string,
-  dob: React.PropTypes.string,
-  Gender: React.PropTypes.string,
-  Email: React.PropTypes.string
+  profileImage:  PropTypes.string,
+  name:          PropTypes.string,
+  jobtitle:      PropTypes.string,
+  dateofjoining: PropTypes.string,
+  dob:           PropTypes.string,
+  gender:        PropTypes.string,
+  work_email:    PropTypes.string,
+  inventory:     PropTypes.bool
+};
+UserHorizontalView.defaultProps = {
+  profileImage:  null,
+  name:          null,
+  jobtitle:      null,
+  dateofjoining: null,
+  dob:           null,
+  gender:        null,
+  work_email:    null,
+  inventory:     false
 };
 export default UserHorizontalView;
