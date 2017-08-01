3983da4529328d0302431394d4c813bcfee404b5
goldentechie <bendam1207@gmail.com>
2017-08-01 18:21:44 +0530

    added accordion and fixed css for myDocuments

--- src/components/holidays/HolidaysList.jsx
+++ src/components/holidays/HolidaysList.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as _ from 'lodash';
 import PropTypes from 'prop-types';
 
 const HolidaysList = ({holidays}) => {
@@ -10,33 +9,35 @@ const HolidaysList = ({holidays}) => {
         <td>{holiday.date}</td>
         <td>{holiday.name}</td>
       </tr>
-    );
+    )
   });
   return (
     <div className="row">
       <div className="col-12">
         <div className="box">
-          <div className="box-divider m-a-0"></div>
-          <table key='' className="table table-striped">
-            <thead className="success">
-              <tr>
-                <th>Month</th>
-                <th>Date</th>
-                <th>Holiday</th>
-              </tr>
-            </thead>
-            <tbody>
-              {holidaysList}
-            </tbody>
-          </table>
+          <div className="box">
+            <div className="box-divider m-a-0"></div>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Month</th>
+                  <th>Date</th>
+                  <th>Holiday</th>
+                </tr>
+              </thead>
+              <tbody>
+                {holidaysList}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 HolidaysList.PropTypes = {
   holidays: PropTypes.array.isRequired
-};
+}
 
 export default HolidaysList;
