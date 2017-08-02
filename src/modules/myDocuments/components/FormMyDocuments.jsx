809960197493f867538fefcecb2925a627d188f4
goldentechie <bendam1207@gmail.com>
2017-08-02 13:15:51 +0530

    My Leave Button

--- src/modules/myDocuments/components/FormMyDocuments.jsx
+++ src/modules/myDocuments/components/FormMyDocuments.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as _ from 'lodash';
 import {CONFIG} from 'src/config/index';
 import {notify} from 'src/services/notify';
 import {getToken} from 'src/services/generic';
@@ -25,13 +26,13 @@ class FormMyDocuments extends React.Component {
     let stop = false;
     if (type === '') {
       stop = true;
-      notify('Warning!', 'Please select document type', 'warning');
+      notify('Please select document type');
     } else if (link1 === '') {
       stop = true;
-      notify('Warning!', 'Please select a file', 'warning');
+      notify('Please select a file');
     } else if (this.refs.declear.checked !== true) {
       stop = true;
-      notify('Warning!', 'Mark declearation before submit', 'warning');
+      notify('Mark declearation before submit');
     }
     if (stop) {
       e.preventDefault();
@@ -40,71 +41,73 @@ class FormMyDocuments extends React.Component {
   deleteDocument (docId) {
     this.props.onDeleteDocument(docId).then((msg) => {
       this.props.onGetMydocuments();
-      notify('Success!', msg.toString(), 'success');
+      notify(msg.toString());
     }).catch((err) => {
-      notify('Error!', err.toString(), 'error');
+      notify(err.toString());
     });
   }
   render () {
-    toggleCollapse();
-    $(window).resize(function () {
-      toggleCollapse();
-    });
-    function toggleCollapse () {
-      if ($(window).width() > 767) {
-        $('#uploadDoc').addClass('in').removeClass('p-b-md');
-        $('#uploadMyDoc').removeClass('md-btn md indigo').css('transition', '.2s');
-      } else {
-        $('#uploadDoc').removeClass('in').addClass('p-b-md');
-        $('#uploadMyDoc').addClass('md-btn md indigo').css('transition', '.2s');
-      }
-    }
+    let styles = _.cloneDeep(this.constructor.styles);
+
     let userId = this.props.user_id;
     let pageUrl = window.location.href;
     return (
-      <div className="row">
-        <div className="col-sm-6 p-x-md p-t-md">
-          <h6 id="uploadMyDoc" className="text-center pointer" data-toggle="collapse" data-target="#uploadDoc">Upload New Documents</h6>
-          <form action={CONFIG.upload_url} method="POST" encType="multipart/form-data" className="collapse" id="uploadDoc">
-            <div className="form-group">
-              <label>Document Type</label>
-              <select className="form-control" ref="doc_type" onChange={() => this.setState({doc_type: this.refs.doc_type.value})} value={this.state.doc_type} >
-                <option value=""> --- Select Doc Type ---</option>
-                <option value="CV">CV</option>
-                <option value="PAN Card">PAN Card</option>
-                <option value="Address Proof">Address Proof</option>
-                <option value="Photo">Photo</option>
-                <option value="Offer Letter">Offer Letter</option>
-                <option value="Appointment Letter">Appointment Letter</option>
-                <option value="Previous Company Experience Letter">Previous Company Experience Letter</option>
-                <option value="Previous Company Offer Letter">Previous Company Offer Letter</option>
-                <option value="Previous Company Salary Slip">Previous Company Salary Slip</option>
-                <option value="Previous Company Other Documents">Previous Company Other Documents</option>
-                <option value="Qualification Certificate">Qualification Certificate</option>
-                <option value="Other Documents">Other Documents</option>
-              </select>
-            </div>
-            <input type="hidden" name="token" value={this.state.user_token} />
-            <input type="hidden" name="user_id" value={userId} />
-            <input type="hidden" name="document_type" value={this.state.doc_type} />
-            <input type="hidden" name="page_url" value={pageUrl} />
-            <div className="form-group">
-              <label>Attachment </label>
-              <input type="file" className="form-control" ref="file" name="link_1" />
-            </div>
-            <div className="form-group">
-              <input type="checkbox" ref="declear" />
-              <span> <b>*IMPORTANT:</b> By uploading this document you certify that these document are true and all information is correct</span>
-            </div>
-            <div className="form-group">
-              <input type="submit" name="submit" value="Upload" className="col-xs-12 md-btn md-raised indigo" onClick={(e) => { this.callUpdateDocuments(e); }} />
-            </div>
-          </form>
+      <div>
+        <div className="row">
+          <div className="col-xs-6">
+            <h6 className="text-center">Upload New Documents</h6>
+            <form action={CONFIG.upload_url} method="POST" encType="multipart/form-data">
+              <div className="form-group">
+                <label>Document Type</label>
+                <select className="form-control" ref="doc_type" onChange={() => this.setState({doc_type: this.refs.doc_type.value})} value={this.state.doc_type} >
+                  <option value="">---select doc type----</option>
+                  <option value="CV">CV</option>
+                  <option value="PAN Card">PAN Card</option>
+                  <option value="Address Proof">Address Proof</option>
+                  <option value="Photo">Photo</option>
+                  <option value="Offer Letter">Offer Letter</option>
+                  <option value="Appointment Letter">Appointment Letter</option>
+                  <option value="Previous Company Experiance Letter">Previous Company Experiance Letter</option>
+                  <option value="Previous Company Offer Letter">Previous Company Offer Letter</option>
+                  <option value="Previous Company Salary Slip">Previous Company Salary Slip</option>
+                  <option value="Previous Company Other Documents">Previous Company Other Documents</option>
+                  <option value="Qualification Certificate">Qualification Certificate</option>
+                  <option value="Other Documents">Other Documents</option>
+                </select>
+              </div>
+              <input type="hidden" name="token" value={this.state.user_token} />
+              <input type="hidden" name="user_id" value={userId} />
+              <input type="hidden" name="document_type" value={this.state.doc_type} />
+              <input type="hidden" name="page_url" value={pageUrl} />
+              <div className="form-group">
+                <label>Attachment </label>
+                <input type="file" className="form-control" ref="file" name="link_1" />
+              </div>
+              <div className="form-group">
+                <input style={styles.checkbox} type="checkbox" ref="declear" />
+                <span style={styles.declearation}><b>*IMPORTANT:</b> By uploading this document you certify that these document are true and all information is correct</span>
+              </div>
+              <div className="form-group">
+                <input type="submit" name="submit" value="Upload" className="col-xs-12 md-btn md-raised indigo" onClick={(e) => { this.callUpdateDocuments(e); }} />
+              </div>
+            </form>
+          </div>
+          <ListDocuments myDocuments={this.props.my_documents} deleteDocument={this.deleteDocument} />
         </div>
-        <ListDocuments myDocuments={this.props.my_documents} deleteDocument={this.deleteDocument} />
       </div>
     );
   }
 }
+
+FormMyDocuments.styles = {
+  checkbox: {
+    verticalAlign: 'middle'
+  },
+  declearation: {
+    display:    'inline-flex',
+    width:      '90%',
+    marginLeft: '10px'
+  }
+};
 
 export default FormMyDocuments;
