b4c8cc8a877664ca446feb71d23a47ef1ebde8b1
goldentechie <bendam1207@gmail.com>
2017-08-05 10:50:30 +0530

    responsive code for 3 Components update password, profile, bank details

--- src/modules/myProfile/containers/myProfile.jsx
+++ src/modules/myProfile/containers/myProfile.jsx
@@ -84,19 +84,23 @@ class MyProfile extends React.Component {
           <div className="app-body" id="view">
             <div className="padding">
               <div className="row no-gutter">
-                <UserHorizontalView profileImage={this.props.loggedUser.data.profileImage} name={this.state.user_profile_detail.name} jobtitle={this.state.user_profile_detail.jobtitle} dateofjoining={this.state.user_profile_detail.dateofjoining} gender={this.state.user_profile_detail.gender} dob={this.state.user_profile_detail.dob} work_email={this.state.user_profile_detail.work_email} />
+                <UserHorizontalView
+                  profileImage={this.props.loggedUser.data.profileImage}
+                  name={this.state.user_profile_detail.name}
+                  jobtitle={this.state.user_profile_detail.jobtitle}
+                  dateofjoining={this.state.user_profile_detail.dateofjoining}
+                  gender={this.state.user_profile_detail.gender}
+                  dob={this.state.user_profile_detail.dob}
+                  workEmail={this.state.user_profile_detail.work_email}
+                />
               </div>
               <div className="row no-gutter">
-                <div className="col-xs-6 p-t p-r b-r">
-                  <FormProfileDetails user_profile_detail={this.state.user_profile_detail} callUpdateProfileDetails={this.callUpdateProfileDetails} />
-                  <br /><br /><br />
-                  <FormUpdatePassword callUpdatePassword={this.callUpdatePassword} />
-                  <br /><br /><br />
-                </div>
-                <div className="col-xs-6 p-t p-l">
-                  <FormBankDetails user_bank_detail={this.state.user_bank_detail} callUpdateBankDetails={this.callUpdateBankDetails} />
-                  <br /><br /><br />
+                <FormProfileDetails user_profile_detail={this.state.user_profile_detail} callUpdateProfileDetails={this.callUpdateProfileDetails} />
+                <FormBankDetails user_bank_detail={this.state.user_bank_detail} callUpdateBankDetails={this.callUpdateBankDetails} />
+                <FormUpdatePassword callUpdatePassword={this.callUpdatePassword} />
+                <div className="col-sm-6 p-a">
                   <h6 className="text-center">Previous Payslips</h6>
+                  <hr />
                   <PayslipHistory payslip_history={this.state.payslip_history} />
                 </div>
               </div>
