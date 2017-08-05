f786e4c64bbc443d9cd8a4f8ecd9785c354e2416
goldentechie <bendam1207@gmail.com>
2017-08-05 15:28:48 +0530

    disable user error message issue resoled

--- src/modules/myProfile/components/FormBankDetails.jsx
+++ src/modules/myProfile/components/FormBankDetails.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import * as _ from 'lodash';
 import PropTypes from 'prop-types';
 import 'react-date-picker/index.css';
-import {ButtonRaised} from 'components/generic/buttons/index';
 
 class FormBankDetails extends React.Component {
   constructor (props) {
@@ -25,6 +24,7 @@ class FormBankDetails extends React.Component {
       });
     }
   }
+
   render () {
     return (
       <div>
@@ -60,17 +60,23 @@ class FormBankDetails extends React.Component {
             ref="ifsc" onChange={(e) => this.setState({ifsc: e.target.value})}
             value={this.state.ifsc} />
         </div>
-        <ButtonRaised className="col-xs-12 indigo" onClick={() => this.props.callUpdateBankDetails(this.state)} label="Update Bank Details" />
+        <button className="col-xs-12 md-btn md-raised indigo" onClick={() => this.props.callUpdateBankDetails(this.state)} >
+          Update Bank Details
+        </button>
       </div>
     );
   }
 }
 
+FormBankDetails.styles = {
+  leaveDiv: {
+    'marginBottom': '10px'
+  }
+};
 FormBankDetails.PropTypes = {
   bank_account_no: PropTypes.number,
   bank_name:       PropTypes.string,
   bank_address:    PropTypes.string,
   ifsc:            PropTypes.string
 };
-
 export default FormBankDetails;
