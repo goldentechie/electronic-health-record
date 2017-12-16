29f8c1af4b682fbad8fa87140e61bf584171bb92
goldentechie <bendam1207@gmail.com>
2017-12-16 12:27:25 +0530

    Created helper for sign, created user constant

--- cypress/integration/admin/login.js
+++ cypress/integration/admin/login.js
@@ -1,26 +1,20 @@
 import { urls, visitIndexRoute, urlVisited } from '../../index';
 
 describe('Test Login Form.', () => {
-  it('should not go to other page when left blank username/password', () => {
+  it('testing login form on various condition', () => {
     visitIndexRoute();
     cy.get('#openLogin').click();
     urlVisited(urls.baseUrl+urls.login);
     cy.get('#loginButton').click();
     urlVisited(urls.baseUrl+urls.login);
-  });
-  it('should not go to other page when left blank username/password', () => {
     cy.get('#usernameInput').type('arun');
     cy.get('#loginButton').click();
     urlVisited(urls.baseUrl+urls.login);
-  });
-  it('when enter wrong data show error', () => {
     cy.get('#usernameInput').type('wrongusername');
     cy.get('#passwordInput').type('wrongpassword');
     cy.get('#loginButton').click();
     cy.get('.sweet-alert').wait(3000).should('be.visible');
     cy.get('.confirm').click();
-  });
-  it('login should success', () => {
     cy.get('#usernameInput').clear().type('arun');
     cy.get('#passwordInput').clear().type('java@123');
     cy.get('#loginButton').click().wait(3000);
