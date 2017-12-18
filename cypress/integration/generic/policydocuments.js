cafedb194010588860cbedf32c5573cc85cdca51
goldentechie <bendam1207@gmail.com>
2017-12-18 19:09:59 +0530

    holiday page test case

--- cypress/integration/generic/policydocuments.js
+++ cypress/integration/generic/policydocuments.js
@@ -2,7 +2,4 @@ describe('Test policy documents', () => {
   it('policy documents list is not empty', () => {
 
   });
-  it('click on any document url will open a new tab, fire an api and status from red change to green or will be green if already green', () => {
-
-  });
 });
