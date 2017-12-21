6a2e2762c63357532fea30201d06e184efce65d6
goldentechie <bendam1207@gmail.com>
2017-12-21 15:14:00 +0530

    added on postinstall script in pacakge.json & readme updated withe error

--- cypress/visitRoutes.js
+++
@@ -1,9 +0,0 @@
-import { urls } from './index';
-
-export const urlVisited = (url) => {
-  cy.url().should('eq',url);
-};
-
-export const visitIndexRoute = () => {
-  cy.visit(urls.baseUrl);
-};
--- src/components/generic/buttons/Button.jsx
+++ src/components/generic/buttons/Button.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({id, type, label, style, className, onClick}) => {
+const Button = ({type, label, style, className, onClick}) => {
   let classname = 'btn ' + className;
   return (
-    <button id={id} type={type} className={classname} style={style} onTouchTap={onClick}>{label}</button>
+    <button type={type} className={classname} style={style} onTouchTap={onClick}>{label}</button>
   );
 };
 
 Button.PropTypes = {
-  id: PropTypes.string,
   type: PropTypes.string,
   label: PropTypes.string.isRequired,
   style: PropTypes.object,
