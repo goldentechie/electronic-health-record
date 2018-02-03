e6a382ea47f73125bbc785141a3c9fb5ae21e873
goldentechie <bendam1207@gmail.com>
2018-02-03 14:55:06 +0530

    search text box will filter the listing test

--- cypress/helper.js
+++ cypress/helper.js
@@ -1,14 +1,25 @@
-import { user, visitIndexRoute, urlVisited, urls } from './index';
+import {urlVisited, visitIndexRoute} from './visitRoutes';
+import {urls} from './index';
 
-export const signin = ( user )=>{
-  visitIndexRoute();
+export const signin = (user) => {
   cy.get('#openLogin').click();
-  urlVisited(urls.baseUrl+urls.login);
+  urlVisited(urls.baseUrl + urls.login);
   cy.get('#usernameInput').type(user.username);
   cy.get('#passwordInput').type(user.password);
   cy.get('#loginButton').click();
-}
-export const signout = () =>{
+};
+export const signout = () => {
   cy.get('#Logout').click();
   urlVisited(urls.baseUrl);
-}
+};
+
+export const apiCall = (method, url, body, testcase) => {
+  let token = null;
+  return new Promise((reslove, reject) => {
+    return cy.request(method, url, body).then((response) => {
+      token = response.body.data.token;
+      reslove(token);
+    });
+  });
+  console.log(token, localStorage.getItem('token'), '================');
+};
