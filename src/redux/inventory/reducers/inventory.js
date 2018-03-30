bf688b6588d68b34cd4e9203f6c42b02a7903e7b
goldentechie <bendam1207@gmail.com>
2018-03-30 10:50:41 +0530

    Working on adding unassigned device list

--- src/redux/inventory/reducers/inventory.js
+++ src/redux/inventory/reducers/inventory.js
@@ -1,11 +1,13 @@
 import Immutable from 'immutable';
 
 let initialState = {
-  'device':          [],
-  'status_message':  '',
-  'deviceList':      [],
-  'statusList':      [],
-  'deviceCountList': {}
+  'device':               [],
+  'status_message':       '',
+  'deviceList':           [],
+  'statusList':           [],
+  'deviceCountList':      {},
+  'comment':              [],
+  'unassignedDeviceList': []
 };
 export function manageDevice (state = Immutable.fromJS(initialState), action) {
   if (action.type === 'ACTION_SUCCESS_DEVICE_LIST') {
@@ -32,6 +34,12 @@ export function manageDevice (state = Immutable.fromJS(initialState), action) {
     return state.set('deviceCountList', action.payload);
   } else if (action.type === 'ACTION_SUCCESS_GET_DEVICE_STATUS_LIST') {
     return state.set('statusList', action.payload);
+  } else if (action.type === 'ACTION_SUCCESS_ADD_USER_COMMENT') {
+    return state.set('comment', action.payload);
+  } else if (action.type === 'ACTION_ERROR_ADD_USER_COMMENT') {
+    return state.set('comment', action.payload);
+  } else if (action.type === 'ACTION_SUCCESS_UNASSIGNED_DEVICE_LIST') {
+    return state.set('comment', action.payload);
   } else {
     return state;
   }
