32090cfb005c4d48a034a802ad21fb684bb70bbb
goldentechie <bendam1207@gmail.com>
2018-04-16 15:21:56 +0530

    InventoryItem design issue fixed and css routes fixed

--- src/modules/inventory/components/inventoryItem.js
+++ src/modules/inventory/components/inventoryItem.js
@@ -12,6 +12,7 @@ import * as actionsManageDevice from "appRedux/inventory/actions/inventory";
 import * as actionsUsersList from "appRedux/generic/actions/usersList";
 import * as actionsManageUsers from "appRedux/manageUsers/actions/manageUsers";
 import ButtonRaised from "components/generic/buttons/ButtonRaised";
+import DialogUpload from "./dialogueUpload";
 let device_id;
 class InventoryItem extends React.Component {
   constructor(props) {
@@ -115,7 +116,7 @@ class InventoryItem extends React.Component {
         );
       }
     );
-    let path = CONFIG.inventory_images
+    let path = CONFIG.inventory_images;
     return (
       <div>
         <Menu {...this.props} />
@@ -125,7 +126,7 @@ class InventoryItem extends React.Component {
             <div className="col-12">
               <div className="app-body" id="view">
                 <div className="col-xs-12 col-sm-12">
-                  <div className="col-md-5 p-r">
+                  <div className="col-md-6 p-r">
                     <div
                       className="form-group"
                       style={{
@@ -134,34 +135,39 @@ class InventoryItem extends React.Component {
                         textAlign: "left"
                       }}
                     >
-                      {" "}
-                      {pat === null || undefined ? null : (
+                      <div className="row">
+                        {" "}
+                        {pat === null || undefined ? null : (
+                          <div className="col-md-12">
+                            <img src={path + pat} />
+                          </div>
+                        )}
+                        <br />
                         <div className="col-md-12">
-                          <img src={path + pat} />
+                          <label style={{ fontSize: 15 }}>Device Name:</label>{" "}
+                          {_.isEmpty(machineName)
+                            ? null
+                            : machineName[0].machine_name}
                         </div>
-                      )}<br />
-                      <div className="col-md-5">
-                        <label style={{ fontSize: 15 }}>Device Name:</label>{" "}
-                        {_.isEmpty(machineName)
-                          ? null
-                          : machineName[0].machine_name}
-                      </div>
-                      <div className="col-md-6">
-                        <label style={{ fontSize: 15 }}>Device Type:</label>{" "}
-                        {_.isEmpty(machineName)
-                          ? null
-                          : machineName[0].machine_type}
-                      </div>
-                      <br />
-                      <div className="col-md-6">
-                        <label style={{ fontSize: 15 }}>Status:</label>{" "}
-                        {_.isEmpty(machineName) ? null : machineName[0].status}
-                      </div>
-                      <div className="col-md-6">
-                        <label style={{ fontSize: 15 }}>Serial No:</label>{" "}
-                        {_.isEmpty(machineName)
-                          ? null
-                          : machineName[0].serial_number}
+                        <div className="col-md-6">
+                          <label style={{ fontSize: 15 }}>Device Type:</label>{" "}
+                          {_.isEmpty(machineName)
+                            ? null
+                            : machineName[0].machine_type}
+                        </div>
+                        <br />
+                        <div className="col-md-6">
+                          <label style={{ fontSize: 15 }}>Status:</label>{" "}
+                          {_.isEmpty(machineName)
+                            ? null
+                            : machineName[0].status}
+                        </div>
+                        <div className="col-md-12">
+                          <label style={{ fontSize: 15 }}>Serial No:</label>{" "}
+                          {_.isEmpty(machineName)
+                            ? null
+                            : machineName[0].serial_number}
+                        </div>
                       </div>
                       <br />
                       <label style={{ fontSize: 15 }}>Users:</label>
@@ -224,6 +230,7 @@ class InventoryItem extends React.Component {
                       </div>
                     </div>
                   </div>
+                  {/* <div className="col-md-5 p-r col-sm-offset-1" style={{marginTop:'17px'}}>{<DialogUpload inventory_id={this.props.routeParams.id} {...this.props}/>}</div> */}
                 </div>
               </div>
             </div>
