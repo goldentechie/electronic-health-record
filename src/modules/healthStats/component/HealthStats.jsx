15a57fabb76d0b38d64349a1db49fa53739519c8
goldentechie <bendam1207@gmail.com>
2018-09-18 16:10:45 +0530

    minor bug fixes

--- src/modules/healthStats/component/HealthStats.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default class HealthStats extends React.Component {
-    render() {
-        return (
-            <div>here</div>
-        )
-    }
-}
