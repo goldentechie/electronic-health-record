a12229a5114543b4a6c47f6a73b43fa550e16524
goldentechie <bendam1207@gmail.com>
2018-11-26 19:30:19 +0530

    updated about 10 route for migration to CRA

--- src/modules/addDocuments/container/AddDocuments.jsx
+++ src/modules/addDocuments/container/AddDocuments.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import Menu from "components/generic/Menu";
-import { isNotUserValid } from "src/services/generic";
+import Menu from "../../../components/generic/Menu";
+import { isNotUserValid } from "../../../services/generic";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import * as actions from "appRedux/actions";
-import * as actionsUsersList from "appRedux/generic/actions/usersList";
+import * as actions from "../../../redux/actions";
 import UsersList from "../../../components/generic/UsersList";
-import Header from "components/generic/Header";
+import Header from "../../../components/generic/Header";
 import FormAddDocuments from "../components/FormAddDocuments";
 
 class AddDocuments extends Component {
